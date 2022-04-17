75ec08ecc5c8c165a3000111b27b1008e3774433
aryanbagoria <70791052+AryanBagoria@users.noreply.github.com>
2022-04-17 13:53:16 +0530

    Update app.js

--- web/public/app.js
+++ web/public/app.js
@@ -1,9 +1,3 @@
-/****
-   SIT209 - Software Engineering 2: Developing IoT Applications
-   Exercise:       Basic Front End
-   Student Name:   Ereena Bagga
-   Student ID:     2010993040 
- ****/
 
 // Load the navbar.html file anytime there is a div mounted to the DOM with the id navbar
 $('#navbar').load('navbar.html');
